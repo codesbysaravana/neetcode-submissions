4ef298eea5f5538b7116dd0b18aa1878d5be7c7e
saravana priyan <68546346+codesbysaravana@users.noreply.github.com>
2026-05-18 19:55:53 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        for(int i=0; i<nums.length; i++) {
+            for(int j=i; j<nums.length; j++) {
+                if(nums[i] > nums[j]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+
+        return nums;
+    }
+}
