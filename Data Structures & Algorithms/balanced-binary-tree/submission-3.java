eb4c236807aaa39307a6d9fc609fab1e3ef4ad0d
saravana priyan <68546346+codesbysaravana@users.noreply.github.com>
2026-06-24 11:37:36 +0530

    Add: balanced-binary-tree - submission-3

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-3.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    boolean flag = true;
+
+    public boolean isBalanced(TreeNode root) {
+        dfs(root);
+        return flag;
+    }
+
+    public int dfs(TreeNode node) {
+        if(node == null) {
+            return 0;
+        }
+
+        int leftTree = dfs(node.left);
+        int rightTree = dfs(node.right);
+
+        if(Math.abs(leftTree - rightTree) > 1) {
+            flag = false;
+        } // once imbalance found, foorever false
+        System.out.println(leftTree);
+        System.out.println(rightTree);
+        System.out.println(node);
+        return 1 + Math.max(leftTree, rightTree);
+    }
+}
+
+
+//remeber left = 1 + leftTreeRec(node.left); --->will only keep going in the left 
+// remmber right = 1 + rightTreeRec(node.right -->) no subtrees only right
